91d8d92c9bf222276a5eddf93761395ca534fd24
Msahu201989 <msahu201989@gmail.com>
2022-09-29 21:53:39 +0530

    catalaogue 2

--- catalogue.sh
+++ catalogue.sh
@@ -1,101 +1,48 @@
 LOG_FILE=/tmp/catalogue
 
+statuscheck() {
+  if [ $1 -eq 0 ] ; then
+       echo status = Success
+               else
+                  echo status = Failure
+                  exit 1
+                  fi
+
 echo downloading nodejs
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-if [ $? -eq 0 ] ;
-           then
-              echo status = Success
-           else
-              echo status = Failure
-              exit 1
-              fi
+statuscheck $?
         echo Installing Nodejs
         yum install nodejs -y &>>$LOG_FILE
-        if [ $? -eq 0 ] ;
-                   then
-                      echo status = Success
-                   else
-                      echo status = Failure
-                      exit 1
-                      fi
+statuscheck $?
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ] ; then
   echo add roboshop user
   useradd roboshop &>>$LOG_FILE
-if [ $? -eq 0 ] ;
-                   then
-                      echo status = Success
-                   else
-                      echo status = Failure
-                      exit 1
-                      fi
+statuscheck $?
         fi
 echo download catalogue app code
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG_FILE
- if [ $? -eq 0 ] ;
-                   then
-                      echo status = Success
-                   else
-                      echo status = Failure
-                      exit 1
-                      fi
+statuscheck $?
         cd /home/roboshop
         echo removing content from ctalogue
         rm -rf catalogue &>>$LOG_FILE
-         if [ $? -eq 0 ] ;
-                                   then
-                                      echo status = Success
-                                   else
-                                      echo status = Failure
-                                      exit 1
-                                      fi
+       statuscheck $?
         echo unzipping catalogue
         unzip /tmp/catalogue.zip &>>$LOG_FILE
-        if [ $? -eq 0 ] ;
-                           then
-                              echo status = Success
-                           else
-                              echo status = Failure
-                              exit 1
-                              fi
+      statuscheck $?
              echo moving catalogue data
         mv catalogue-main catalogue &>>$LOG_FILE
-         if [ $? -eq 0 ] ;
-                                   then
-                                      echo status = Success
-                                   else
-                                      echo status = Failure
-                                      exit 1
-                                      fi
+         statuscheck $?
         cd /home/roboshop/catalogue &>>$LOG_FILE
         echo installing dependencies
                npm install &>>$LOG_FILE
-                if [ $? -eq 0 ] ;
-                                          then
-                                             echo status = Success
-                                          else
-                                             echo status = Failure
-                                             exit 1
-                                             fi
+            statuscheck $?
              echo setting up service in systemd
              mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>$LOG_FILE
              echo daemon reload starting ctalogue service
              systemctl daemon-reload
-              if [ $? -eq 0 ] ;
-                                                       then
-                                                          echo status = Success
-                                                       else
-                                                          echo status = Failure
-                                                          exit 1
-                                                          fi
-
+              statuscheck $?
                  systemctl start catalogue &>>$LOG_FILE
              systemctl enable catalogue &>>$LOG_FILE
-           if [ $? -eq 0 ] ;
-                      then
-                         echo status = Success
-                      else
-                         echo status = Failure
-                         exit 1
-                         fi
+       statuscheck $?
