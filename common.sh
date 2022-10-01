8455cc504b8b819da54c142ef14cc6190fa36f52
Msahu201989 <msahu201989@gmail.com>
2022-10-01 22:54:52 +0530

    user

--- common.sh
+++ common.sh
@@ -8,3 +8,51 @@ statuscheck() {
                   exit 1
                   fi
                   }
+
+ NODEJS () {
+   echo downloading nodejs
+   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
+   statuscheck $?
+           echo Installing Nodejs
+           yum install nodejs -y &>>$LOG_FILE
+   statuscheck $?
+
+   id roboshop &>>$LOG_FILE
+   if [ $? -ne 0 ] ; then
+     echo add roboshop user
+     useradd roboshop &>>$LOG_FILE
+   statuscheck $?
+           fi
+   echo download ${COMPONENT} app code
+   curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/${COMPONENT}/main.zip" &>>$LOG_FILE
+   statuscheck $?
+           cd /home/roboshop
+           echo removing content from ctalogue
+           rm -rf ${COMPONENT} &>>$LOG_FILE
+          statuscheck $?
+           echo unzipping ${COMPONENT} APP CODE
+           unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
+         statuscheck $?
+                echo moving catalogue data
+           mv ${COMPONENT}-main ${COMPONENT} &>>$LOG_FILE
+            statuscheck $?
+           cd /home/roboshop/${COMPONENT} &>>$LOG_FILE
+           echo installing dependencies
+                  npm install &>>$LOG_FILE
+               statuscheck $?
+
+     echo Updating Systemd service file
+     sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+     statuscheck $?
+
+
+      echo setting up ${COMPONENT} service in systemd
+                   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+                   echo daemon reload starting ${COMPONENT} service
+                   systemctl daemon-reload
+                    statuscheck $?
+                       systemctl start ${COMPONENT} &>>$LOG_FILE
+                   systemctl enable ${COMPONENT} &>>$LOG_FILE
+           statuscheck $?
+
+ }
--- user.sh
+++ user.sh
@@ -1,51 +1,9 @@
 LOG_FILE=/tmp/catalogue
 
 source common.sh
+COMPONENT=user
 
 
-echo We Need NodeJS
-echo Downloading NodeJS
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
-statuscheck $?
+Nodejs
 
-
-echo Installing NodeJS NOW
-yum install nodejs -y &>>$LOG_FILE
-statuscheck $?
-
-echo Creating Roboshop User
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ] ; then
-  echo add roboshop user
-  useradd roboshop &>>$LOG_FILE
-statuscheck $?
-        fi
-
- echo downloading Robohop conent
- curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip" &>>$LOG_FILE
- statuscheck $?
-
-
-echo Installing NodeJS service
- cd /home/roboshop &>>$LOG_FILE
- unzip /tmp/user.zip &>>$LOG_FILE
- statuscheck $?
- mv user-main user &>>$LOG_FILE
- cd /home/roboshop/user &>>$LOG_FILE
- npm install &>>$LOG_FILE
- statuscheck $?
-
-echo Updating Systemd service file
-sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' /home/roboshop/user/systemd.service &>>$LOG_FILE
-statuscheck $?
-
-
- echo setting up USER service in systemd
-              mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service &>>$LOG_FILE
-              echo daemon reload starting ctalogue service
-              systemctl daemon-reload
-               statuscheck $?
-                  systemctl start user &>>$LOG_FILE
-              systemctl enable user &>>$LOG_FILE
-      statuscheck $?
 
