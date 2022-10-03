d46f74b4e174c7a621ab4f3b4453c56fdcb318fa
Msahu201989 <msahu201989@gmail.com>
2022-10-03 12:20:43 +0530

    user

---
+++ MYSQL.sh
@@ -0,0 +1,23 @@
+LOG_FILE=/tmp/mysql
+source common.sh
+
+echo setting up MY SQL repo
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>LOG_FILE
+statuscheck $?
+
+echo Disable Mysql Default Module to enable 5.7 MYQL
+dnf module disable mysql &>>LOG_FILE
+statuscheck $?
+
+echo Installing Mysql
+yum install mysql-community-server -y &>>LOG_FILE
+statuscheck $?
+
+echo Starting MYQL service
+systemctl enable mysqld &>>LOG_FILE
+statuscheck $?
+systemctl start mysqld &>>LOG_FILE
+statuscheck $?
+
+
+
