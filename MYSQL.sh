02e93bb644606c0f1f44a06693cc213fe9516233
Msahu201989 <msahu201989@gmail.com>
2022-10-03 19:49:57 +0530

    user

--- MYSQL.sh
+++ MYSQL.sh
@@ -19,14 +19,9 @@ statuscheck $?
 systemctl start mysqld &>>LOG_FILE
 statuscheck $?
 
-DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+#DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${ROBOSHOP_MYSQL_PASSWORD}');
-FLUSH PRIVILEGES;" >/tmp/root-pass.sql
 
-echo "Change the Default password"
-mysql --connect-expired-password -uroot -p"${DEFAULT_PASSWORD}" </tmp/root-pass.sql $>>$LOG_FILE
-statuscheck $?
 
 
 
