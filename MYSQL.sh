33e51578462c74f5a99ad2513fca3a91d69ce5b1
Msahu201989 <msahu201989@gmail.com>
2022-10-03 22:16:28 +0530

    my sql

--- MYSQL.sh
+++ MYSQL.sh
@@ -19,7 +19,6 @@ statuscheck $?
 systemctl start mysqld &>>LOG_FILE
 statuscheck $?
 
-
 echo "seting up password"
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 statuscheck $?
