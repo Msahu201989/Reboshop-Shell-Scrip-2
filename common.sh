0c2f0035bdab1245120dd40c8e301f5531e568ef
Msahu201989 <msahu201989@gmail.com>
2022-10-07 19:28:42 +0530

    user

--- common.sh
+++ common.sh
@@ -9,6 +9,45 @@ statuscheck() {
                   fi
                   }
 
+APP_PREREQ() {
+     id roboshop &>>$LOG_FILE
+     if [ $? -ne 0 ] ; then
+       echo add roboshop user
+       useradd roboshop &>>$LOG_FILE
+     statuscheck $?
+             fi
+     echo download ${COMPONENT} app code
+     curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+     statuscheck $?
+             cd /home/roboshop
+             echo removing content from ctalogue
+             rm -rf ${COMPONENT} &>>$LOG_FILE
+            statuscheck $?
+             echo unzipping ${COMPONENT} APP CODE
+             unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
+           statuscheck $?
+                  echo moving catalogue data
+             mv ${COMPONENT}-main ${COMPONENT} &>>$LOG_FILE
+              statuscheck $?
+             cd /home/roboshop/${COMPONENT} &>>$LOG_FILE
+}
+SYSTEMD_SETUP(){
+      echo Updating Systemd service file
+       sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/'/home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
+       statuscheck $?
+
+
+        echo setting up ${COMPONENT} service in systemd
+                     mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+                     echo daemon reload starting ${COMPONENT} service
+                     systemctl daemon-reload
+                      statuscheck $?
+                         systemctl start ${COMPONENT} &>>$LOG_FILE
+                     systemctl enable ${COMPONENT} &>>$LOG_FILE
+             statuscheck $?
+
+}
+
  NODEJS () {
    echo downloading nodejs
    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
@@ -16,43 +55,29 @@ statuscheck() {
            echo Installing Nodejs
            yum install nodejs -y &>>$LOG_FILE
    statuscheck $?
+APP_PREREQ
 
-   id roboshop &>>$LOG_FILE
-   if [ $? -ne 0 ] ; then
-     echo add roboshop user
-     useradd roboshop &>>$LOG_FILE
-   statuscheck $?
-           fi
-   echo download ${COMPONENT} app code
-   curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
-   statuscheck $?
-           cd /home/roboshop
-           echo removing content from ctalogue
-           rm -rf ${COMPONENT} &>>$LOG_FILE
-          statuscheck $?
-           echo unzipping ${COMPONENT} APP CODE
-           unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
-         statuscheck $?
-                echo moving catalogue data
-           mv ${COMPONENT}-main ${COMPONENT} &>>$LOG_FILE
-            statuscheck $?
-           cd /home/roboshop/${COMPONENT} &>>$LOG_FILE
            echo installing dependencies
                   npm install &>>$LOG_FILE
                statuscheck $?
 
-     echo Updating Systemd service file
-     sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongo.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'/home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE
-     statuscheck $?
+ SYSTEMD_SETUP
+ }
+
+ JAVA() {
+   echo "INSTALL MAVEN"
+   yum install maven -y &>>${LOG_FILE}
+   statuscheck $?
+   APP_PREREQ
+
+   echo "download dependencies and make packages "
 
 
-      echo setting up ${COMPONENT} service in systemd
-                   mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
-                   echo daemon reload starting ${COMPONENT} service
-                   systemctl daemon-reload
-                    statuscheck $?
-                       systemctl start ${COMPONENT} &>>$LOG_FILE
-                   systemctl enable ${COMPONENT} &>>$LOG_FILE
-           statuscheck $?
+ mvn clean package &>>${LOG_FILE}
+ mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
+  statuscheck $?
+
+SYSTEMD_SETUP
+
 
  }
