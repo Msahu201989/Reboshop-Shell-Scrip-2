0b76a151dbc2f2b9b593a2c24a02c46f0bf635f6
Msahu201989 <msahu201989@gmail.com>
2022-10-07 21:51:24 +0530

    user

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -15,9 +15,12 @@ systemctl enable rabbitmq-server &>>LOG_FILE
 systemctl start rabbitmq-server &>>LOG_FILE
 statuscheck $?
 
+rabbitmqctl list_users | grep roboshop &>>LOG_FILE
+if [$? -ne 0]; then
 echo "Add app user in rabbitmq"
 rabbitmqctl add_user roboshop roboshop123 &>>LOG_FILE
 statuscheck $?
+fi
 
 echo "add app user tag in rabbitmq"
 rabbitmqctl set_user_tags roboshop administrator &>>LOG_FILE
