536deb51d3d1932882453739da6ada6f8f7f6bf4
Msahu201989 <msahu201989@gmail.com>
2022-10-07 19:33:02 +0530

    user

--- Shipping.sh
+++ Shipping.sh
@@ -1,22 +1,10 @@
+COMPONENT=SHIPPING
+LOG_FILE=/tmp/${COMPONENT}
+source common.sh
 
 
 
-echo "Installing Maven as we need JAVA"
 
-yum install maven -y &>>LOG_FILE
-
-echo "AS per process Adding user robohop to run this app"
-useradd roboshop
-
-echo "downloading repo"
-cd /home/roboshop
-curl -s -L -o /tmp/shipping.zip "https://github.com/roboshop-devops-project/shipping/archive/main.zip"
-echo "unzipping data"
-unzip /tmp/shipping.zip
-$ mv shipping-main shipping
-$ cd shipping
-$ mvn clean package
-$ mv target/shipping-1.0.jar shipping.jar
 
 
 
