4d3e461221b713530184a6a4befaa19cb23f3abe
Msahu201989 <msahu201989@gmail.com>
2022-10-07 23:03:59 +0530

    user

--- mongodb.sh
+++ mongodb.sh
@@ -10,32 +10,32 @@ echo installing mongodb
 yum install -y mongodb-org &>>$LOG_FILE
 statuscheck $?
 
-echo enabling mongodb and starting mongodb
-systemctl enable mongod &>>$LOG_FILE
-systemctl start mongod &>>$LOG_FILE
 
-statuscheck $?
 
 echo updating ipaddress for mongodb
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 statuscheck $?
 
-  echo restarting mongodb
-  systemctl restart mongod &>>$LOG_FILE
-    statuscheck $?
-
+echo enabling mongodb and starting mongodb
+systemctl enable mongod &>>$LOG_FILE
+systemctl restart mongod &>>$LOG_FILE
+statuscheck $?
 
       echo downloading Database Schema
       curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
       statuscheck $?
 
-              cd /tmp
-              echo unzip mongodb
-              unzip mongodb.zip &>>$LOG_FILE
-             statuscheck $?
+ cd /tmp
+  echo unzip mongodb
+  unzip mongodb.zip &>>$LOG_FILE
+ statuscheck $?
 
-                      echo lodaing schema database
-              cd mongodb-main
-              mongo < catalogue.js &>>$LOG_FILE
-              mongo < users.js &>>$LOG_FILE
+cd mongodb-main
+
+    echo lodaing catalogue schema
+    mongo < catalogue.js &>>$LOG_FILE
+    statuscheck $?
+
+    echo  load user schema
+  mongo < users.js &>>$LOG_FILE
 statuscheck $?
