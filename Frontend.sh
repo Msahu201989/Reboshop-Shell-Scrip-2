61d6f42bcc060d389ee73354f2b9a176460e7a0c
Msahu201989 <msahu201989@gmail.com>
2022-10-07 22:36:17 +0530

    user

--- Frontend.sh
+++ Frontend.sh
@@ -27,7 +27,9 @@ mv frontend-main/static/* . &>>$LOG_FILE
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 statuscheck $?
 
+echo "update roboshop config files"
 sed -i -e '/catalogue s/localhost/catalogue.roboshop.internal/' -e '/user s/localhost/user.roboshop.internal/' -e '/cart/ s/localhost/cart.roboshop.internal/' -e '/payment/ s/localhost/payment.roboshop.internal/' -e '/shipping s/localhost/shipping.roboshop.internal/' /etc/nginx//default.d/roboshop.conf
+statuscheck $?
 
 
 echo restarting nginx service
